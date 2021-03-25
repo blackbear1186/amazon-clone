4f05a93f6992fca4e6aef7d508592b2ca631c39d
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-25 22:04:50 +0900

    got nav set up

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import {GoSearch} from 'react-icons/go'
+import {RiArrowDropDownFill} from 'react-icons/ri'
+import {CgShoppingCart} from 'react-icons/cg'
 
 const Header = () => {
   return (
-    <div className="header">
+    <nav className="header">
       <img
         className="amazon_logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
@@ -16,11 +18,29 @@ const Header = () => {
         <span className="address-text">Yokohama-Shi 245-0063</span>
       </div>
       <div className='search-box'>
+        <div className='all'>
+          <span>
+          All
+          </span>
+          <RiArrowDropDownFill className='drop-down-arrow'/>
+          </div>
         <input type='text'></input>
-        <GoSearch />
-
+        <div className='search-icon-div'>
+        <GoSearch className='search-icon'/>
+        </div>
       </div>
-    </div>
+      <div>
+        <span>Hello, Berlin</span>
+        <span>Account & Lists</span>
+      </div>
+      <div>
+        <span>Returns</span>
+        <span>& Orders</span>
+      </div>
+      <div>
+        <CgShoppingCart />
+      </div>
+    </nav>
   );
 };
 
--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 const Navigation = () => {
   return (
     <nav className='navbar'>
-      
+      <div className='ham-div'>
+      <GiHamburgerMenu className='hamburger-menu'/>
+      <span>All</span>
+      </div>
+      <span className='second-nav-text'>Today's Deals</span>
+      <span className='second-nav-text'>Berlin's Amazon.com</span>
+      <span className='second-nav-text'>Customer Service</span>
+      <span className='second-nav-text'>Browsing History</span>
+      <span className='second-nav-text'>Buy Again</span>
+      <span className='second-nav-text'>Gift Cards</span>
+      <span className='second-nav-text'>Sell</span>
+      <span className='second-nav-text'>Amazon's Response to Covid-19</span>
+
+
     </nav>
   )
 }
