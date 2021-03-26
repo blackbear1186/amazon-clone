fc6dcc7004a3b75968e2dad7ab672ccf435b59aa
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-26 22:06:12 +0900

    Set up home

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      This is home
+    <div className="home-container">
+      <img className='main-image' src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_2x._CB432458382_.jpg" alt="main-image"></img>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
